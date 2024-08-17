7cd6b71726eb37665f111af150c04234fa20724f
King Lucifer <119492378+LuciferDevOp@users.noreply.github.com>
2024-08-17 21:08:16 +0530

    Initial Commit

---
+++ setLink.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { url } = require('inspector');
+
+function extractSubdomain(filePath) {
+  const urlRegex = /https:\/\/([^.]+)\.trycloudflare\.com/;
+
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const lines = data.split('\n');
+
+    for (const line of lines) {
+      const match = line.match(urlRegex);
+      if (match) {
+        return match[1];
+      }
+    }
+
+    return null;
+  } catch (error) {
+    console.error("Error reading file:", error);
+    return null;
+  }
+}
+
+async function linkToDB(link) {
+  const databaseUrl = "https://mybot-md2-default-rtdb.firebaseio.com/link.json";
+  const data = link;
+  const response2 = await fetch(databaseUrl, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' }
+  });
+  console.log(response2);
+}
+
+const filePath = 'output.txt';
+const subdomain = extractSubdomain(filePath);
+const link = `https://${subdomain}.trycloudflare.com`;
+linkToDB(link);
